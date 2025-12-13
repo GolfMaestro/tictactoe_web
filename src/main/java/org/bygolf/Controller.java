a5d201fbc7b45f3746a98e058ae497162c8bf0f7
Vlad Eryn <vlad2004e@gmail.com>
2025-12-13 15:46:45 +0300

    basic setup

---
+++ src/main/java/org/bygolf/Controller.java
@@ -0,0 +1,14 @@
+package org.bygolf;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/main")
+    public String mainPage() {
+        return "Tic tac toe app";
+    }
+
+}

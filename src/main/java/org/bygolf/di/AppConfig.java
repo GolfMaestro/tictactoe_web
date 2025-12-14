bf28d2e0e7b9795ded66c1951e1c2eecf8660e20
Vlad Eryn <vlad2004e@gmail.com>
2025-12-14 16:39:49 +0300

    update app config

--- src/main/java/org/bygolf/di/AppConfig.java
+++ src/main/java/org/bygolf/di/AppConfig.java
@@ -1,8 +1,16 @@
 package org.bygolf.di;
 
+import org.bygolf.datasource.mapper.DSToDomain;
+import org.bygolf.datasource.mapper.DSToDomainImpl;
+import org.bygolf.datasource.mapper.DomainToDS;
+import org.bygolf.datasource.mapper.DomainToDSImpl;
 import org.bygolf.datasource.repository.Storage;
 import org.bygolf.domain.ai.BotAi;
 import org.bygolf.domain.ai.BotAiImpl;
+import org.bygolf.web.mapper.DomainToWeb;
+import org.bygolf.web.mapper.DomainToWebImpl;
+import org.bygolf.web.mapper.WebToDomain;
+import org.bygolf.web.mapper.WebToDomainImpl;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +28,26 @@ public class AppConfig {
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public Storage storage() {
         return new Storage();
+    }
+
+    @Bean
+    public DSToDomain dsToDomainMapper() {
+        return new DSToDomainImpl();
+    }
+
+    @Bean
+    public DomainToDS domainToDSMapper() {
+        return new DomainToDSImpl();
+    }
+
+    @Bean
+    public WebToDomain webToDomainMapper() {
+        return new WebToDomainImpl();
+    }
+
+    @Bean
+    public DomainToWeb domainToWebMapper() {
+        return new DomainToWebImpl();
     }
 
 }

3f037271681062438fedf9ea6adc3ec96ac0f08a
Vlad Eryn <vlad2004e@gmail.com>
2025-12-14 11:59:44 +0300

    added datasource models

---
+++ src/main/java/org/bygolf/datasource/model/DSCurrentGame.java
@@ -0,0 +1,24 @@
+package org.bygolf.datasource.model;
+
+import org.bygolf.domain.model.GameField;
+
+import java.util.UUID;
+
+public class DSCurrentGame {
+
+    private UUID id;
+    private DSGameField dsGameField;
+
+    public DSCurrentGame(UUID id, DSGameField dsGameField) {
+        this.id = id;
+        this.dsGameField = dsGameField;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public DSGameField getDsGameField() {
+        return dsGameField;
+    }
+}
---
+++ src/main/java/org/bygolf/datasource/model/DSGameField.java
@@ -0,0 +1,16 @@
+package org.bygolf.datasource.model;
+
+public class DSGameField {
+
+    int[][] field;
+
+    public DSGameField(int[][] field) {
+        this.field = field;
+    }
+
+    public int[][] getField() {
+        return field;
+    }
+
+
+}
